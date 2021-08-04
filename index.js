d6d26b09f3c4b52bbf424d0a79fa47ce1c81132b
Tim <serazutdinov.tim@gmail.com>
2021-08-04 21:23:28 +0300

    Better POST login

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 const port = process.env.PORT || 8888
+
+const users = [
+    {id: 1, username: 'admin', password: 'admin'},
+    {id: 2, username: 'guest', password: 'guest'}
+]
 
 app.use(bodyParser.json())
 
@@ -15,11 +21,32 @@ app.get('/status', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    const user = req.body.username
+    if (!req.body.username || !req.body.password) {
+        res
+        .status(400)
+        .send('You need a username and password!')
+        return  
+    }
+
+    const user = users.find((u) => {
+        return u.username === req.body.username && u.password === req.body.password
+    })
+
+    if (!user) {
+        res
+        .status(401)
+        .send('User wasn\'t found')
+        return        
+    }
+
+    const token = jwt.sign({
+        sub: user.id,
+        username: user.username
+    }, 'mysupersecretkey', {expiresIn: "3 hours"})
 
     res
     .status(200)
-    .send(`You logged in with ${user}`)
+    .send({access_token: token});
 })
 
 app.get('*', (req, res) => {
