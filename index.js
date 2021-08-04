4b696a4c15d6ddd7e7d4c29c88f4a5d7d7bb628e
Tim <serazutdinov.tim@gmail.com>
2021-08-04 14:55:21 +0300

    Simple Login post

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT || 8888
+
+app.use(bodyParser.json())
 
 app.get('/status', (req, res) => {
     const localTime = (new Date()).toLocaleTimeString()
@@ -9,6 +12,14 @@ app.get('/status', (req, res) => {
     res
     .status(200)
     .send(`Server time is ${localTime}.`)
+})
+
+app.post('/login', (req, res) => {
+    const user = req.body.username
+
+    res
+    .status(200)
+    .send(`You logged in with ${user}`)
 })
 
 app.get('*', (req, res) => {
