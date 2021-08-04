562e5e7c3b488d60809ed172514598e4fc6b24e2
Tim <serazutdinov.tim@gmail.com>
2021-08-04 21:32:13 +0300

    Cors

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const jwt = require('jsonwebtoken')
+const jwt = require('express-jwt')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 8888
+
+const jwtCheck = jwt({
+    secret: 'mysecret'
+})
 
 const users = [
     {id: 1, username: 'admin', password: 'admin'},
@@ -11,6 +16,7 @@ const users = [
 ]
 
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/status', (req, res) => {
     const localTime = (new Date()).toLocaleTimeString()
@@ -20,34 +26,47 @@ app.get('/status', (req, res) => {
     .send(`Server time is ${localTime}.`)
 })
 
-app.post('/login', (req, res) => {
-    if (!req.body.username || !req.body.password) {
-        res
-        .status(400)
-        .send('You need a username and password!')
-        return  
-    }
-
-    const user = users.find((u) => {
-        return u.username === req.body.username && u.password === req.body.password
-    })
-
-    if (!user) {
-        res
-        .status(401)
-        .send('User wasn\'t found')
-        return        
-    }
-
-    const token = jwt.sign({
-        sub: user.id,
-        username: user.username
-    }, 'mysupersecretkey', {expiresIn: "3 hours"})
-
+app.get('/resource', (req, res) => {
     res
     .status(200)
-    .send({access_token: token});
+    .send('Public resoure, u can see this')
 })
+
+app.get('/resource/secret', jwtCheck, (req, res) => {
+    res
+    .status(200)
+    .send('Secret resoure, u can see this if u logged in')
+})
+
+
+// app.post('/login', (req, res) => {
+//     if (!req.body.username || !req.body.password) {
+//         res
+//         .status(400)
+//         .send('You need a username and password!')
+//         return  
+//     }
+
+//     const user = users.find((u) => {
+//         return u.username === req.body.username && u.password === req.body.password
+//     })
+
+//     if (!user) {
+//         res
+//         .status(401)
+//         .send('User wasn\'t found')
+//         return        
+//     }
+
+//     const token = jwt.sign({
+//         sub: user.id,
+//         username: user.username
+//     }, 'mysupersecretkey', {expiresIn: "3 hours"})
+
+//     res
+//     .status(200)
+//     .send({access_token: token});
+// })
 
 app.get('*', (req, res) => {
     res.sendStatus(404)
